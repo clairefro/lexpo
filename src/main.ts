66fcb1bfd5e40dd9c4e04251b3187eb6383af991
Claire Froelich <claire.froelich@gmail.com>
2021-11-14 19:20:05 -0800

    Fix scaling aspect ratio on window resize

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,45 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from "./scenes/HelloWorldScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
-}
+  type: Phaser.WEBGL,
+  scale: {
+    mode: Phaser.Scale.ScaleModes.RESIZE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+      debug: false,
+    },
+  },
+  callbacks: {
+    postBoot: () => {
+      window.sizeChanged();
+    },
+  },
+  canvasStyle: `display: block; width: 100%; height: 100%;`,
+  autoFocus: true,
+  scene: [HelloWorldScene],
+};
 
-export default new Phaser.Game(config)
+window.sizeChanged = () => {
+  console.log("resize");
+  if (window?.game?.isBooted) {
+    setTimeout(() => {
+      window.game.scale.resize(window.innerWidth, window.innerHeight);
+
+      window.game.canvas.setAttribute(
+        "style",
+        `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`
+      );
+    }, 100);
+  }
+};
+
+window.onresize = () => window.sizeChanged();
+
+export default new Phaser.Game(config);
